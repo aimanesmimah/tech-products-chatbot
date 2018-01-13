a77bb8a1577416f8faa50658043dcaea935ee4a9
cloudstrife9494 <aymansmimah@yahoo.fr>
2018-01-13 19:11:03 +0000

    setting up a node.js project using express framework and adding socket.io & api.ai node sdk libraries as well as testing web speech API

---
+++ public/javascripts/script.js
@@ -0,0 +1,37 @@
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+const socket = io();
+
+function synthVoice(text) {
+    const synth = window.speechSynthesis;
+    const utterance = new SpeechSynthesisUtterance();
+    utterance.text = text;
+    synth.speak(utterance);
+}
+
+
+document.querySelector('button').addEventListener('click',() => {
+    console.log('hi');
+    recognition.start();
+});
+
+
+
+recognition.addEventListener('result',(e)=>{
+   let last = e.results.length - 1 ;
+   let text = e.results[last][0].transcript;
+
+   console.log('Confidence: ' + e.results[0][0].confidence);
+
+
+   socket.emit('chat_message',text);
+
+    $('#message').html("You say :" +text);
+});
+
+socket.on('bot_reply', function(replyText) {
+    synthVoice(replyText);
+
+    $('#answer').html("You get : " + replyText);
+});
