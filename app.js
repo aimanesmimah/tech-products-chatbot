e7bfed11a76e44d751d38948023e75e646a10122
cloudstrife9494 <aymansmimah@yahoo.fr>
2018-01-19 14:24:33 +0000

    adding http cors

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var webhook = require('./webhook/webhook');
 var app = express();
 
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -23,6 +24,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use('/', index);
 app.use('/users', users);
