a43b45b33a5c2ec567774f63afce192f87340dba
cloudstrife9494 <aymansmimah@yahoo.fr>
2018-02-17 00:39:30 +0000

    updating cors headers

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ var users = require('./routes/users');
 var app = express();
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
